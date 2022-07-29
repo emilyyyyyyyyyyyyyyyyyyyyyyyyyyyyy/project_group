42e15297b04399bd2ef3b97f96caa357d799773d
emilyyyyyyyyyyyyyyyyyyyyyyyyyyyyy <s10244258@connect.np.edu.sg>
2022-07-29 22:10:15 +0800

    done

--- profit_loss.py
+++ profit_loss.py
@@ -8,3 +8,5 @@ def profitloss_function(forex):
     with file.open(mode= 'a', encoding = 'UTF-8', newline = '') as text:
         with fp.open(mode="r", newline='') as pal:
             next(pal)
+            prevday=0
+            npdiff=0
