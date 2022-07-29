2323f16e5b182f9617b39e31a6feba1be3d9179e
emilyyyyyyyyyyyyyyyyyyyyyyyyyyyyy <s10244258@connect.np.edu.sg>
2022-07-29 21:59:11 +0800

    done

--- profit_loss.py
+++ profit_loss.py
@@ -1,4 +1,9 @@
 from pathlib import Path
 import re
 
-fp = Path.cwd()/"csv_report"/"profit-and-loss-usd.csv"
+fp = Path.cwd()/"csv_reports"/"profit-and-loss-usd.csv"
+file = Path.cwd()/"csv_reprts"/"summary_report.txt"
+
+def profitloss_function(forex):
+    with file.open(mode= 'a', encoding = 'UTF-8', newline = '') as text:
+#        with fp.open(mode="r", newline=''):
