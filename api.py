c530528dd16e755b6f4ca712deea7147302e4f66
shaniceeeeeeeeeeee <s10243707@connect.np.edu.sg>
2022-07-30 23:53:08 +0800

    done

--- api.py
+++ api.py
@@ -17,7 +17,7 @@ def api_function():
         # Get the api url
 
         api = json.dumps(api, indent=4)
-        # Make the output neater
+        # Arrange the output in a neat way
         pattern = 'Exchange Rate": ".+'
         api = re.search(pattern, api)
         ER = api.group(0).strip('Exchange Rate":"').strip('",')
