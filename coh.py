daf87af5adc4fb587b31ea8aaf2f2bda542e2aef
Yus-Airah <s10247296@connect.np.edu.sg>
2022-07-31 00:08:29 +0800

    done

--- coh.py
+++ coh.py
@@ -5,4 +5,7 @@ fp = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
 file = Path.cwd()/"csv_reports"/"summary_report.txt"
 
 def coh_function(forex):
-    
+    with file.open(mode = 'a', encoding = 'UTF-8', newline = '') as text:
+        with fp.open(mode="r", newline='') as coh:
+            next(coh)
+            prevday=0
