c1704aea59f2ccf59e67b0a3aff24ffdaccd1d00
Yus-Airah <s10247296@connect.np.edu.sg>
2022-07-30 23:18:04 +0800

    done

--- coh.py
+++ coh.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import re
 
 fp = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
+# Create a file path to cash-on-hand-usd.csv
 file = Path.cwd()/"csv_reports"/"summary_report.txt"
 
 def coh_function(forex):
