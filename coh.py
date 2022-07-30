25701fc310af920ad1295f080b575489ecbeb55f
Yus-Airah <s10247296@connect.np.edu.sg>
2022-07-30 23:12:46 +0800

    done

--- coh.py
+++ coh.py
@@ -2,3 +2,9 @@ from pathlib import Path
 import re
 
 fp = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
+file = Path.cwd()/"csv_reports"/"summary_report.txt"
+
+def coh_function(forex):
+    with file.open(mode = 'a', encoding = 'UTF-8', newline = '') as text:
+        with fp.open(mode="r", newline='') as coh:
+            next(coh)
