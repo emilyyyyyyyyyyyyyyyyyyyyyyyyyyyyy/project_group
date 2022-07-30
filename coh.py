1204b795d0744edf66c3f2af8e78dc1a1392093f
Yus-Airah <s10247296@connect.np.edu.sg>
2022-07-30 23:21:28 +0800

    done

--- coh.py
+++ coh.py
@@ -4,13 +4,19 @@ import re
 fp = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
 # Create a file path to cash-on-hand-usd.csv
 file = Path.cwd()/"csv_reports"/"summary_report.txt"
+# Create a file path to file summary_report
 
 def coh_function(forex):
+    # Create a function
     with file.open(mode = 'a', encoding = 'UTF-8', newline = '') as text:
+        # Open the summary_report as text file
         with fp.open(mode="r", newline='') as coh:
+            # Open the cash-on-hand-usd csv file
             next(coh)
+            # Skip the header
             prevday=0
             cohdiff=0
+            # Let variables prevday and cohdiff be 0
 
             for line in coh.readlines():
                 line = re.findall(r'[0-9]+.', line)
