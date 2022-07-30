9e05677fa29c897222666100e3f7dfa0ff1109b3
Yus-Airah <s10247296@connect.np.edu.sg>
2022-07-30 23:14:30 +0800

    done

--- coh.py
+++ coh.py
@@ -8,3 +8,10 @@ def coh_function(forex):
     with file.open(mode = 'a', encoding = 'UTF-8', newline = '') as text:
         with fp.open(mode="r", newline='') as coh:
             next(coh)
+            prevday=0
+            cohdiff=0
+
+            for line in coh.readlines():
+                line = re.findall(r'[0-9]+.', line)
+                cohdiff = float(line[1]) - prevday
+                prevday = float(line[1])
