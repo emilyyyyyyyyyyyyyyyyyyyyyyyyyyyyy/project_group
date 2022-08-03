0915ec1832a4985890dd78f1c6c7c9968cf0311e
shaniceeeeeeeeeeee <s10243707@connect.np.edu.sg>
2022-08-03 12:24:41 +0800

    done

--- api.py
+++ api.py
@@ -21,7 +21,7 @@ def api_function():
         pattern = 'Exchange Rate": ".+'
         api = re.search(pattern, api)
         ER = api.group(0).strip('Exchange Rate":"').strip('",')
-        # Extract the exchange rate
+        # Extract the exchange rat
 
 
         ER = float(ER)
