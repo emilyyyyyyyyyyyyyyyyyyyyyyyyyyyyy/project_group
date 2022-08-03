4aa5fe3e1f859fe316be0b5ff474b8775e7443af
emilyyyyyyyyyyyyyyyyyyyyyyyyyyyyy <s10244258@connect.np.edu.sg>
2022-08-03 22:27:36 +0800

    exception handling

--- overheads.py
+++ overheads.py
@@ -9,35 +9,38 @@ file = Path.cwd()/"csv_reports"/"summary_report.txt"
 
 def overhead_function(forex):
 # Create a function 
-# function requires parameter: forex
     with file.open(mode = 'a', encoding = 'UTF-8', newline = '') as text:
     # Open the summary_report text file 
-        with file_path.open(mode='r', newline='') as overheads:
-        # Open the file
-            next(overheads)
-            # Skip the headers
-            max_value = float(0)
-            # Let variable max_value be float 0
+        try:
+            with file_path.open(mode='r', newline='') as overheads:
+            # Open the file
+                next(overheads)
+                # Skip the headers
+                max_value = float(0)
+                # Let variable max_value be float 0
+    
+                try:
+                    for line in overheads.readlines():
+                    # Read overheads-day-41.csv line by line 
+                        line = line.split(",")
+                        # Split the line at ","
+                        category =  line[0].strip('"').strip('"').upper()
+                        # Find the respective categories
+                        overheads = re.findall(r'[0-9].+[0-9].+' , line[1]) 
+                        # Find the respective overhead values
+                        a = float(overheads[0])
+                        # Let variable a be the overhead values
+    
+    
+                        if a > max_value:
+                        # If a is more than max_value
+                            max_value = a
+                            # a will overwrite the old value, and become the new max value 
+                            text.writelines(f"[HIGHEST OVERHEADS] {category}, EXPENSE: SGD{max_value*forex}\n")
+                            # Write [HIGHEST OVERHEADS] with the variable category, EXPENSE: SGD with the variable max_value with f strings
+                
+                except IndexError:
+                    print("The data is not a number")
 
-            try:
-                for line in overheads.readlines():
-                # Read overheads-day-41.csv line by line 
-                    line = line.split(",")
-                    # Split the line at ","
-                    category =  line[0].strip('"').strip('"').upper()
-                    # Find the respective categories
-                    overheads = re.findall(r'[0-9].+[0-9].+' , line[1]) 
-                    # Find the respective overhead values
-                    a = float(overheads[0])
-                    # Let variable a be the overhead values
-
-
-                    if a > max_value:
-                    # If a is more than max_value
-                        max_value = a
-                        # a will overwrite the old value, and become the new max value 
-                        text.writelines(f"[HIGHEST OVERHEADS] {category}, EXPENSE: SGD{max_value*forex}\n")
-                        # Write [HIGHEST OVERHEADS] with the variable category, EXPENSE: SGD with the variable max_value with f strings
-            
-            except IndexError:
-                print("The data is not a number")
+        except FileNotFoundError:
+            print("The file is unavailable")
--- profit_loss.py
+++ profit_loss.py
@@ -10,39 +10,42 @@ def profitloss_function(forex):
 # Create a function
     with file.open(mode= 'a', encoding = 'UTF-8', newline = '') as text:
     # Open the summary_report text file
-        with fp.open(mode="r", newline='') as pal:
-        # Open the file
-            next(pal)
-            # Skip the header
-            prevday=0
-            npdiff=0
-            # Let variables prevday and paldiff be 0
-            surplus=1
-            # Let variable surplus be 1
+        try:
+            with fp.open(mode="r", newline='') as pal:
+            # Open the file
+                next(pal)
+                # Skip the header
+                prevday=0
+                npdiff=0
+                # Let variables prevday and paldiff be 0
+                surplus=1
+                # Let variable surplus be 1
 
-            for line in pal.readlines():
-                try:
-                # Read profit-and-loss-usd.csv line by line
-                    line = re.findall(r'[0-9]+.', line)
-                    # Find all the net profit data
-                    npdiff = float(line[4]) - prevday
-                    # Make npdiff be the line of data minus the previous line of data for net profit in profit and loss
-                    prevday = float(line[4])
-                    # Make prevday be the line of data for net profit in profit and loss
-                    
-                    if npdiff <0:
-                    # If npdiff is less than 0
-                        npdiff = abs(npdiff)
-                        # Make it positive
-                        text.writelines(f'[PROFIT DEFICIT] DAY: {line[0]} AMOUNT: SGD{npdiff*forex}\n')
-                        # Print [PROFIT DEFICIT] DAY: {line[0]} AMOUNT: SGD{npdiff*forex}
-                        surplus=0
-                        # Then variable surplus will be 0
-                    
-                except IndexError:
-                    print("The data is not a number")
+                for line in pal.readlines():
+                    try:
+                    # Read profit-and-loss-usd.csv line by line
+                        line = re.findall(r'[0-9]+.', line)
+                        # Find all the net profit data
+                        npdiff = float(line[4]) - prevday
+                        # Make npdiff be the line of data minus the previous line of data for net profit in profit and loss
+                        prevday = float(line[4])
+                        # Make prevday be the line of data for net profit in profit and loss
+                        
+                        if npdiff <0:
+                        # If npdiff is less than 0
+                            npdiff = abs(npdiff)
+                            # Make it positive
+                            text.writelines(f'[PROFIT DEFICIT] DAY: {line[0]} AMOUNT: SGD{npdiff*forex}\n')
+                            # Print [PROFIT DEFICIT] DAY: {line[0]} AMOUNT: SGD{npdiff*forex}
+                            surplus=0
+                            # Then variable surplus will be 0
+                        
+                    except IndexError:
+                        print("The data is not a number")
 
-            if surplus==1:
-                # If surplus is really 1
-                    text.writelines('[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n')
-                    # Write [NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY
+                if surplus==1:
+                    # If surplus is really 1
+                        text.writelines('[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n')
+                        # Write [NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY
+        except FileNotFoundError:
+                print("The file is unavailable")
