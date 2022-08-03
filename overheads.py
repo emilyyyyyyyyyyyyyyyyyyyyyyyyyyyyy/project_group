f7196a0e7731cdb68248cadfbde9886d9aabfaaa
genevieveeeeee <s10240115@connect.np.edu.sg>
2022-08-03 21:46:38 +0800

    done

--- overheads.py
+++ overheads.py
@@ -8,9 +8,8 @@ file = Path.cwd()/"csv_reports"/"summary_report.txt"
 
 
 def overhead_function(forex):
-# Create a function 
-"""
-one parameter required: forex
+# Create a function that requires forex as a parameter
+
     with file.open(mode = 'a', encoding = 'UTF-8', newline = '') as text:
     # Open the summary_report text file 
         with file_path.open(mode='r', newline='') as overheads:
